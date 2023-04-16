37a65dbe547c4f764b188190d4a1554d60cf02f1
Zofikar1 <pawel.chwalczyk@gmail.com>
2023-04-16 18:39:28 +0200

    Fixed link issues

--- src/Components/NotesContainer.tsx
+++ src/Components/NotesContainer.tsx
@@ -16,7 +16,7 @@ type Props = {
 export default function NotesContainer({notebook, removeNotes, addNote, removeNote, onTitlesChanged, onNoteChange}: Props){
     return (
         <div id={`Notes${notebook.id}`} className='p-5 bg-gray-600 rounded-lg min-w-[10rem]'>
-            <div className="flex items-center grow justify-between flex-nowrap">
+            <div className="flex items-center grow justify-between flex-nowrap gap-2">
                 <Editable className='text-white  text-2xl bg-gray-600' value={notebook.title} parent_id={notebook.id} grandpa_id={-1} onChangeFunction={onTitlesChanged} ></Editable>
                 <FontAwesomeIcon icon={icon({name:"minus", style:"solid"})} className="text-white bg-gray-700 h-6 w-6 border-solid border-1 border-gray-700 rounded-md" onClick={()=> removeNotes(notebook.id)}/>
             </div>
